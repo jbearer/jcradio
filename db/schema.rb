c94f2a1c65926078b06cecf51f7e8c162e052e6e
Aurora Leeson <aleeson@g.hmc.edu>
2020-04-25 14:51:57 -0700

    Added background image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200425201022) do
+ActiveRecord::Schema.define(version: 20200425210237) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "source"
