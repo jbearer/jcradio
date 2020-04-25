6f048bce0d77bd434c55c49305f68b9aca1a2ff5
Daniel Zhang <danielz@ghs.com>
2020-04-25 14:47:33 -0700

    replaying spotify search change without the Gemfile change

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,8 @@ class SongsController < ApplicationController
     # GET /songs/search
     #   query: string of keywords
     def search
-        @songs = Song.fuzzy_search(params[:query].split)
+        @songs = Song.spotify_search(params[:query])
+        #@songs = Song.fuzzy_search(params[:query].split)
             # TODO look up from Spotify; our database is only populated _after_ a
             # song has been searched in Spotify.
         render template: "songs/index"
