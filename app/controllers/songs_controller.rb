ba4e45818deeb968b6a64f9544e1197bd9f8641f
Jeb Bearer <jbearer>
2020-08-17 11:48:06 -0700

    Keep query in search bar

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,12 +2,14 @@ class SongsController < ApplicationController
     # GET /songs
     def index
         @songs = []
+        @query = nil
     end
 
     # GET /songs/search
     #   query: string of keywords
     def search
         @songs = Song.spotify_search(params[:query])
+        @query = params[:query]
         #@songs = Song.fuzzy_search(params[:query].split)
             # TODO look up from Spotify; our database is only populated _after_ a
             # song has been searched in Spotify.
