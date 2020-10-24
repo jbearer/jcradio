87f6dbe1b5d5c1a3adbddb0df0091023821854a7
Austin <achun@g.hmc.edu>
2020-10-24 12:20:51 -0700

    New logins are next in the queue (shifting up/down user positions on login/logout)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,13 +17,35 @@ class SessionsController < ApplicationController
         else
             if @user.station.nil?
                 station = Station.find 1
+
+                # Push back all other users
+                User.where("position > ?", station.users.minimum(:position) || -1).each do |inc_user|
+                  puts "\n\n\n&&&&&&&"
+                  puts inc_user.username, inc_user.position
+                  inc_user.update position: inc_user.position + 1
+                  puts inc_user.username, inc_user.position
+                  puts "\n\n\n&&&&&&&"
+                end
+
                 @user.update station: station,
-                             position: (station.users.maximum(:position) || -1) + 1
+                             position: (station.users.minimum(:position) || -1) + 1
+                puts "\n\n\n&&&&&&&"
+                puts @user.username, @user.position
             else
               if @user.position.nil?
+                # Push back all other users
+                User.where("position > ?", @user.station.users.minimum(:position) || -1).each do |inc_user|
+                  puts "\n\n\n&&&&&&&"
+                  puts inc_user.username, inc_user.position
+                  inc_user.update position:  inc_user.position + 1
+                  puts inc_user.username, inc_user.position
+                  puts "\n\n\n&&&&&&&"
+                end
                 # If we're already a member of a station, but we're not in line to add
                 # songs to that station, join the back of the line.
-                @user.update position: (@user.station.users.maximum(:position) || -1) + 1
+                @user.update position: (@user.station.users.minimum(:position) || -1) + 1
+                puts "\n\n\n&&&&&&&"
+                puts @user.username, @user.position
               end
             end
             session[:user_id] = @user.id
@@ -43,6 +65,17 @@ class SessionsController < ApplicationController
   def destroy
     if logged_in?
         broadcast :push, "#{current_user.username} left the radio."
+
+        # Push back all other users
+        User.where("position > ?", current_user.position).each do |inc_user|
+          puts "\n\n\n&&&&&&&"
+          puts inc_user.username, inc_user.position
+          inc_user.update position: inc_user.position-1
+          puts inc_user.username, inc_user.position
+          puts "\n\n\n&&&&&&&"
+        end
+
+
         current_user.update station: nil, position: nil
         LiveRPC.close current_user.id
         reset_session
