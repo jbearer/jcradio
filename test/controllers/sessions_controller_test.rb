d5718d615e144396964b9cbe4c1f70316bf07ae8
Austin Chun <austinchun21@gmail.com>
2026-09-13 11:02:57 -0700

    Working on tests...

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,50 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+  setup do
+    @user = users(:one)
+    @previous_next_letter = $the_next_letter
+    # A blank queue cannot supply a next letter, so skip that lookup on login.
+    $the_next_letter = 'A'
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  teardown do
+    $the_next_letter = @previous_next_letter
   end
 
+  test "logging in joins the station and starts a session" do
+    post :create, username: @user.username
+
+    assert_response :redirect
+    assert_equal @user.id, session[:user_id]
+    assert_equal stations(:one), @user.reload.station
+    assert_equal 0, @user.position
+  end
+
+  test "logging in as an unknown user does not start a session" do
+    post :create, username: 'nobody'
+
+    assert_response :redirect
+    assert_nil session[:user_id]
+    assert_equal "no such user nobody", flash[:error]
+  end
+
+  test "logging out leaves the station and clears the session" do
+    @user.update station: stations(:one), position: 0
+    session[:user_id] = @user.id
+
+    delete :destroy
+
+    assert_response :redirect
+    assert_nil session[:user_id]
+    assert_nil @user.reload.station
+    assert_nil @user.position
+  end
+
+  test "logging out when not logged in reports an error" do
+    delete :destroy
+
+    assert_response :redirect
+    assert_equal "cannot log out (not logged in)", flash[:error]
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,7 +8,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:users)
+    assert_not_nil assigns(:user)
   end
 
   test "should get new" do
@@ -18,10 +18,18 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { position: @user.position, username: @user.username }
+      post :create, username: 'newcomer'
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to '/sessions'
+  end
+
+  test "should not create a duplicate username" do
+    assert_no_difference('User.count') do
+      post :create, username: @user.username
+    end
+
+    assert_redirected_to '/users/new'
   end
 
   test "should show user" do
@@ -29,21 +37,21 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
+  test "should not destroy user when not logged in" do
+    assert_no_difference('User.count') do
+      delete :destroy, id: @user
+    end
+
+    assert_response :redirect
   end
 
-  test "should update user" do
-    patch :update, id: @user, user: { position: @user.position, username: @user.username }
-    assert_redirected_to user_path(assigns(:user))
-  end
+  test "should destroy own user when logged in" do
+    session[:user_id] = @user.id
 
-  test "should destroy user" do
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
 
-    assert_redirected_to users_path
+    assert_response :redirect
   end
 end
